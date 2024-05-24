1b5286946df7ccabf564ed171c99a3eef6309144
ahps <akbarputrasrg@gmail.com>
2024-05-24 10:15:42 +0700

    course f: mock bean

--- src/test/java/com/charuniverse/springwebmvc/e_service/ServiceTest.java
+++ src/test/java/com/charuniverse/springwebmvc/e_service/ServiceTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloServiceImplTest {
+class ServiceTest {
 
     @Autowired
     private HelloService helloService;
