995c8aaabe95f017f421f1abecdcbfec6e225193
ahps <akbarputrasrg@gmail.com>
2024-05-24 10:47:01 +0700

    course l: request header

---
+++ src/main/java/com/charuniverse/springwebmvc/controller/HeaderController.java
@@ -0,0 +1,20 @@
+package com.charuniverse.springwebmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HeaderController {
+
+    @GetMapping(path = "/header/token")
+    @ResponseBody
+    public String header(@RequestHeader(name = "X-TOKEN") String token) {
+        if (token.equals("gemink")) {
+            return "Token is valid";
+        } else {
+            return "Token is invalid";
+        }
+    }
+}
