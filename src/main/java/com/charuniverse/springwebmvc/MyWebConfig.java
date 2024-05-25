048303bab18472fb4db843c9bd10e16b6ec6c3cb
ahps <akbarputrasrg@gmail.com>
2024-05-25 07:39:17 +0700

    course zb: interceptor

--- src/main/java/com/charuniverse/springwebmvc/MyWebConfig.java
+++ src/main/java/com/charuniverse/springwebmvc/MyWebConfig.java
@@ -1,8 +1,20 @@
 package com.charuniverse.springwebmvc;
 
+import com.charuniverse.springwebmvc.interceptor.SessionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/user/*");
+//        WebMvcConfigurer.super.addInterceptors(registry);
+    }
 }
