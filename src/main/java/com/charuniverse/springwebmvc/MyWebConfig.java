7d54b19be7abee43c41531a3e578ae5c56ec5973
ahps <akbarputrasrg@gmail.com>
2024-05-25 07:30:13 +0700

    course za: web mvc configurer

---
+++ src/main/java/com/charuniverse/springwebmvc/MyWebConfig.java
@@ -0,0 +1,8 @@
+package com.charuniverse.springwebmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+}
